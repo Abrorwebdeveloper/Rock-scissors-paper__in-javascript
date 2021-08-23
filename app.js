13b6d88bacaf74632e43853b9bea19f787e0b411
Abrorwebdeveloper <bogibekovabror5696@gmail.com>
2021-08-23 18:27:18 +0500

    added js code and sccs code

--- app.js
+++ app.js
@@ -15,25 +15,93 @@ let paperBtn = document.querySelector(".paper__btn");
 let scissorsBtn = document.querySelector(".scissors__btn");
 
 let countingBtn = document.querySelectorAll(".count__btn");
-let CountButtonClicks = 0;
 
+// gamer name function
 let gamerNameResult = document.querySelector(".gamer__name");
-
 let gamerName = prompt("Ismingizni kiriting");
+function nameGamer(name) {
+ if(gamerName == ""){
+     
+     alert('Ismingizni kiritishingiz kerak')
+     name = prompt("Ismingizni kiriting")
+     gamerNameResult.innerHTML = `Ishtirokchi ismi: ${name}`;
+ }else{
+    gamerNameResult.innerHTML = `Ishtirokchi ismi: ${name}`;
+ }
+}
+nameGamer(gamerName);
 
-function nameGamer(name){
-    gamerNameResult.innerHTML = `Ishtirokchi ismi: ${name}`
+// rounds counting function
+let CountButtonClicks = 0;
+function counting() {
+  CountButtonClicks += 1;
+  roundsNum.innerHTML = CountButtonClicks;
 }
 
-nameGamer(gamerName)
+for (let i = 0; i < countingBtn.length; i++) {
+  countingBtn[i].addEventListener("click", () => {
+    let imgArr = [
+      "./images/rock.fae7b245.png",
+      "./images/paper.24d69089.png",
+      "./images/scissors.0dc12241.png",
+    ];
+    let randomImgSrc = Math.floor(Math.random() * imgArr.length);
+    let userChoice = imgArr[i];
+    let computerChoice = imgArr[randomImgSrc];
+    let rock = imgArr[0];
+    let paper = imgArr[1];
+    let scissors = imgArr[2];
 
+    if (userChoice == computerChoice) {
+      gamerAndcomputerChoice();
+      return draw();
+    } else if (userChoice == rock) {
+      if (computerChoice == paper) {
+        gamerAndcomputerChoice();
+        return lose();
+      } else if (computerChoice == scissors) {
+        gamerAndcomputerChoice();
+        return win();
+      }
+    } else if (userChoice == paper) {
+      if (computerChoice == rock) {
+        gamerAndcomputerChoice();
+        return win();
+      } else if (computerChoice == scissors) {
+        gamerAndcomputerChoice();
+        return lose();
+      }
+    } else if (userChoice == scissors) {
+      if (computerChoice == paper) {
+        gamerAndcomputerChoice();
+        return win();
+      } else if (computerChoice == rock) {
+        gamerAndcomputerChoice();
+        return lose();
+      }
+    }
 
-function counting(){
-    CountButtonClicks += 1;
-    roundsNum.innerHTML = CountButtonClicks
+    function gamerAndcomputerChoice() {
+      gamerImg.src = userChoice;
+      computerImg.src = computerChoice;
+    }
+  });
 }
 
+let userPoints = 0;
+function win() {
+  userPoints++;
+  gamerResult.innerHTML = userPoints;
+}
 
+let drawPoins = 0;
+function draw() {
+  drawPoins++;
+  tiesResult.innerHTML = drawPoins;
+}
 
-
-
+let losePoints = 0;
+function lose() {
+  losePoints++;
+  computerResult.innerHTML = losePoints;
+}
